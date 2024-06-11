f061c989c08de2d755498e7f031a15f5681c5661
Yartyjung <nattapat.ems@gmail.com>
2024-06-11 19:14:47 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 from transformers import pipeline
-
-pipe = pipeline(task="text-classification",model="yartyjung/Fake-Review-Detector")
-
-st.title(":rainbow[Review-Detector]")
-st.divider()
-text = st.text_input("Your :red[suspicious] review here :sunglasses:",value="")
-@st.experimental_memo(ttl=600)
-if st.button("predict"):
-    if text is not None:
-        predictions = pipe(text)
-        if predictions[0]['label'] == 'fake':
-            for p in predictions:
-                st.subheader(f":red[FAKE]   :blue[{ round(p['score'] * 100, 1)} %]")
-        elif predictions[0]['label'] == 'real':
-            for p in predictions:
-                st.subheader(f":green[REAL]   :blue[{ round(p['score'] * 100, 1)} %]")
-        st.divider()
-        st.markdown(":red[***disclaimer***   This is a prediction by an _AI_, which might turn out incorrect.]")
-        url = "https://huggingface.co/yartyjung/Fake-Review-Detector"
-        st.markdown(":yellow[check out model at this [link](%s)]" % url)
+@st.cache_resource(ttl=300)
+def ai():
+    pipe = pipeline(task="text-classification",model="yartyjung/Fake-Review-Detector")
+    st.title(":rainbow[Review-Detector]")
+    st.divider()
+    text = st.text_input("Your :red[suspicious] review here :sunglasses:",value="")
+    if st.button("predict"):
+        if text is not None:
+            predictions = pipe(text)
+            if predictions[0]['label'] == 'fake':
+                for p in predictions:
+                    st.subheader(f":red[FAKE]   :blue[{ round(p['score'] * 100, 1)} %]")
+            elif predictions[0]['label'] == 'real':
+                for p in predictions:
+                    st.subheader(f":green[REAL]   :blue[{ round(p['score'] * 100, 1)} %]")
+            st.divider()
+            st.markdown(":red[***disclaimer***   This is a prediction by an _AI_, which might turn out incorrect.]")
+            url = "https://huggingface.co/yartyjung/Fake-Review-Detector"
+            st.markdown(":yellow[check out model at this [link](%s)]" % url)
+            ai()
+ai()
