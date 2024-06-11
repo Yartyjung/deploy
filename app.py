8b83be4a2e7884e89d53dbc7cc18bb56ab48ea08
Yartyjung <nattapat.ems@gmail.com>
2024-06-11 19:02:11 +0700

    update app.py

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ st.title(":rainbow[Review-Detector]")
 st.divider()
 text = st.text_input("Your :red[suspicious] review here :sunglasses:",value="")
 
-if text is not None:
-    predictions = pipe(text)
-    if predictions[0]['label'] == 'fake':
-        for p in predictions:
-            st.subheader(f":red[FAKE]   :blue[{ round(p['score'] * 100, 1)} %]")
-    elif predictions[0]['label'] == 'real':
-        for p in predictions:
-            st.subheader(f":green[REAL]   :blue[{ round(p['score'] * 100, 1)} %]")
-    st.divider()
-    st.markdown(":red[***disclaimer***   This is a prediction by an _AI_, which might turn out incorrect.]")
-    url = "https://huggingface.co/yartyjung/Fake-Review-Detector"
-    st.markdown(":yellow[check out model at this [link](%s)]" % url)
+if st.button("predict"):
+    if text is not None:
+        predictions = pipe(text)
+        if predictions[0]['label'] == 'fake':
+            for p in predictions:
+                st.subheader(f":red[FAKE]   :blue[{ round(p['score'] * 100, 1)} %]")
+        elif predictions[0]['label'] == 'real':
+            for p in predictions:
+                st.subheader(f":green[REAL]   :blue[{ round(p['score'] * 100, 1)} %]")
+        st.divider()
+        st.markdown(":red[***disclaimer***   This is a prediction by an _AI_, which might turn out incorrect.]")
+        url = "https://huggingface.co/yartyjung/Fake-Review-Detector"
+        st.markdown(":yellow[check out model at this [link](%s)]" % url)
